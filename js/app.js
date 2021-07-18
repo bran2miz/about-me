e056cbbd775d27d56331038e216e942de9379544
brandonmizutani <bran2miz@gmail.com>
2021-07-17 18:03:51 -0700

    edited all my code

--- js/app.js
+++ js/app.js
@@ -1,88 +1,88 @@
- 'use strict';
+ `use strict`;
 
 // The user's name from the greeting!
-let username= prompt('Might I inquire your name?');
+let username= prompt(`Might I inquire your name?`);
 
 // Greet the user
 alert('Hello ' + username);
 
-let answerOne = prompt('Welcome to this web page! Is this your first time on this page? Please reply with a \'yes\' or \'no\'').toLowerCase();
+let answerOne = prompt(`Welcome to this web page! Is this your first time on this page? Please reply with a yes or no`).toLowerCase();
 
 
-if (answerOne === 'yes' || answerOne === 'y' || answerOne === 'ya') {
+if (answerOne === `yes` || answerOne === `y` || answerOne === `ya`) {
   alert('Pray tell why you would journey back, mortal.');
 }
 
 else if (answerOne === 'no' || answerOne === 'n') {
-  alert('Welcome, Foolish Mortal, to this spooky web page!');
+  alert(`Welcome, Foolish Mortal, to this spooky web page!`);
 }
 
 else {
-  alert('I am sorry, I could not quite catch that. Please respond with a yes or no!');
+  alert(`I am sorry, I could not quite catch that. Please respond with a yes or no!`);
 }
 
 // console.log('user replied to question 1 with: ' + answerOne');
 
-let answerTwo = prompt('There is no turning back now. Our spooky Q&A begins here! Was the name of the killer in the film, \'Halloween\', named Michael Myers? Mortal! Answer now with a \'yes\' or \'no\'').toLowerCase();
+let answerTwo = prompt(`There is no turning back now. Our spooky Q&A begins here! Was the name of the killer in the film, Halloween, named Michael Myers? Mortal! Answer now with a yes or no`).toLowerCase();
 
-if (answerTwo === 'yes' || answerTwo === 'y' || answerTwo === 'ya') {
-  alert('Well, well. You are correct. Begginer\'s\ luck I suppose.');
+if (answerTwo === 'yes' || answerTwo === 'y' || answerTwo === `ya`) {
+  alert(`Well, well. You are correct. Begginer luck I suppose.`);
 }
 
-else if (answerTwo === 'no' || answerTwo === 'n') {
-  alert('Wrong! Perhaps you are more foolish than I thought');
+else if (answerTwo === `no` || answerTwo === `n`) {
+  alert(`Wrong Perhaps you are more foolish than I thought!`);
 }
 
 else {
-  alert('Mortal! Please enter a viable answer! \'yes\' or \'no\'!');
+  alert('Mortal! Please enter a viable answer! yes or no!');
 }
 
 // console.log('user replied to question 2 with: ' + answerTwo');
 
-let answerThree = prompt('Hmm...that was just a warmup question. This is a bunch of hocus pocus... Speaking of which, mortal, did Bette Midler portray the character, Winifred Sanderson, in the movie, \'Hocus Pocus\'? Reply with \'yes\' or \'no\'').toLowerCase();
+let answerThree = prompt(`Hmm...that was just a warmup question. This is a bunch of hocus pocus... Speaking of which, mortal, did Bette Midler portray the character, Winifred Sanderson, in the movie, Hocus Pocus? Reply with yes or no`).toLowerCase();
 
-if (answerThree === 'yes' || answerThree === 'y' || answerThree === 'ya') {
-  alert('Correct! Well, what do you know. Third time really is the charm')
+if (answerThree === `yes` || answerThree === `y` || answerThree === `ya`) {
+  alert(`Correct! Well, what do you know. Third time really is the charm`)
 }
 
 else if (answerThree === 'no' || answerThree === 'n') {
-  alert('Incorrect! Horror, you are not very well versed, because you are \'cursed\'!');
+  alert(`Incorrect! Horror, you are not very well versed, because you are cursed!`);
 }
 
 else {
-  alert('Mortal! Again, please enter a viable answer! \'yes\' or \'no\'!');
+  alert(`Mortal! Again, please enter a viable answer! yes or no!`);
 }
 
 // console.log('user replied to question 3 with: ' + answerThree');
 
-let answerFour = prompt('Nearing the end of our questions. Halloween...Did this holiday originate in Ireland? Reply with \'yes\' or \'no\'').toLowerCase();
+let answerFour = prompt(`Nearing the end of our questions. Halloween...Did this holiday originate in Ireland? Reply with yes or no`).toLowerCase();
 
-if (answerFour === 'yes' || answerFour === 'y' || answerFour === 'ya') {
-  alert('Correct! Im assuming you are well versed in all that is spooky and scary');
+if (answerFour === `yes` || answerFour === `y` || answerFour === `ya`) {
+  alert(`Correct! Im assuming you are well versed in all that is spooky and scary`);
 }
 
-else if (answerFour === 'no' || answerFour === 'n') {
-  alert('Incorrect! You are having bad luck with these questions...aren\'t\ you?');
+else if (answerFour === `no` || answerFour === `n`) {
+  alert(`Incorrect! You are having bad luck with these questions...`);
 }
 
 else {
-  alert('Mortal! Please enter a viable answer! \'yes\' or \'no\'!');
+  alert(`Mortal! Please enter a viable answer! yes or no!`);
 }
 
 // console.log('user replied to question 4 with: ' + answerFour');
 
-let answerFive = prompt('Well our questions end here with this last one. Answer wisely....Was this game enjoyable? Reply with \'yes\' or \'no\'').toLowerCase();
+let answerFive = prompt(`Well our questions end here with this last one. Answer wisely....Was this game enjoyable? Reply with yes or no`).toLowerCase();
 
-if (answerFive === 'yes' || answerFive === 'y' || answerFive === 'ya') {
-  alert('Phew! Mortal, I am glad I didn\'t\ have to curse you! Thank you for answering correctly! Have a pleasant life! Goodbye for now....')
+if (answerFive === `yes` || answerFive === `y` || answerFive === `ya`) {
+  alert(`Phew! Mortal, I am glad I did not have to curse you! Thank you for answering correctly! Have a pleasant life! Goodbye for now....`);
 }
 
-else if (answerFour === 'no' || answerFour === 'n') {
-  alert('You did not like the game...Fine! I won\'t\ curse you, but I will make sure that you enjoy my next game!');
+else if (answerFour === `no` || answerFour === `n`) {
+  alert(`You did not like the game...Fine! I will not curse you, but I will make sure that you enjoy my next game!`);
 }
 
 else {
-  alert('This is the end of the game! Please enter a viable answer! \'yes\' or \'no\'!')
+  alert(`This is the end of the game! Please enter a viable answer! yes or no!`);
 }
 
 //console.log('user replied to question 5 with: ' + answerFive');
@@ -119,31 +119,6 @@ while (guessAttempts) {
     }
   }}
 
-let favDestination = ['Paris', 'Barcelona', 'Cabo', 'Antwerp', 'Vienna'];
-let favWorld = prompt('What is one of my favorite destinations to visit?');
-let attemptedGuess = 5;
-
-While (attemptedGuess) {
-  for (let i = 0; i < attemptedGuess.length; i++) {
-    if (favWorld === favDestination[i]) {
-      attemptedGuess = 0;
-      alert(`You are Correct!`);
-      break;
-    }
-    else if (favWorld !== favDestination[i]){
-      alert(`That is not one of my favorite places to travel. You have ${attemptedGuess} attempts remaining.`)
-      userAnswer = prompt(`Can you try another destination?`);
-    }
-    attemptedGuess--;
-    if (attemptedGuess === 0 && favWorld !== favDestination[i]) {
-      alert(`My favorite colors are: ` + favDestination);
-    }
-    else if (userAnswer= favDestination[i]){alert(`You Got it!!`);
-  }
-
-  }
-}}
-
 let favDestination = [`paris`, `barcelona`, `cabo`];
 let favWorld = prompt('What is one of my favorite destinations to visit?');
 
@@ -157,13 +132,13 @@ while (guessUser) {
       break;
     } 
     else if (favWorld !== favDestination[i]){
-      
-      alert(`That is not one of my favorite colors. You have ${guessUser} attempts remaining.`);
-      favWorld = prompt(`Can you try another color?`);
+
+      alert(`That is not one of my destinations. You have ${guessUser} attempts remaining.`);
+      favWorld = prompt(`Can you try another destination?`);
     }
     guessUser--;
     if (guessUser === 0 && favWorld !== favDestination[i]){
-      alert('My favorite colors are: ' + favDestination);
+      alert('My favorite destinations are: ' + favDestination);
     }
     else if (favWorld === favDestination[i]){
       alert('Wahooo!! You got it!');
@@ -171,12 +146,12 @@ while (guessUser) {
   }
 }
 
-let buildAnswers = [answerOne, answerTwo, answerThree, answerFour, answerFive, userGuess, favWorld];
+// let buildAnswers = [answerOne, answerTwo, answerThree, answerFour, answerFive, userGuess, favWorld];
 
-const output = [];
+// const output = [];
 
-myAnswers.forEach(currentQuestion, questionNumber) => {
-  const answers = [];
+// myAnswers.forEach(currentQuestion, questionNumber) = {
+//   const answers = [];
 
-  for 
-}
+//   for 
+// }
